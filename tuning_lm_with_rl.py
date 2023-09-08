04719e4870cc8b1cc11519ff864639958bf8b0c7
Carro <robertmyers@hey.com>
2023-09-08 18:31:27 -0500

    added revision

--- tuning_lm_with_rl.py
+++ tuning_lm_with_rl.py
@@ -328,14 +328,14 @@ output_length_sampler = LengthSampler(output_min_length, output_max_length)
 for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
     question_tensors = batch["input_ids"]
 
-    # response_tensors = ppo_trainer.generate(
-    #     question_tensors,
-    #     return_prompt=False,
-    #     length_sampler=output_length_sampler,
-    #     **generation_kwargs,
-    # )
+    response_tensors = ppo_trainer.generate(
+        question_tensors,
+        return_prompt=False,
+        length_sampler=output_length_sampler,
+        **generation_kwargs,
+    )
     batch['query'] = tokenizer.batch_decode(question_tensors, skip_special_tokens=True)
-    # batch["response"] = tokenizer.batch_decode(response_tensors, skip_special_tokens=True)
+    batch["response"] = tokenizer.batch_decode(response_tensors, skip_special_tokens=True)
 
     batch["response"] = ["testing" for _ in range(len(batch["query"]))]
 
@@ -356,8 +356,8 @@ for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
     rewards = [torch.tensor(output - script_args.reward_baseline) for output in rewards_pre]
     logger.info("rewards", rewards)
     # Run PPO step
-    # stats = ppo_trainer.step(question_tensors, response_tensors, rewards)
-    # ppo_trainer.log_stats(stats, batch, rewards)
+    stats = ppo_trainer.step(question_tensors, response_tensors, rewards)
+    ppo_trainer.log_stats(stats, batch, rewards)
 
-    # if script_args.save_freq and epoch and epoch % script_args.save_freq == 0:
-    #     ppo_trainer.save_pretrained(script_args.output_dir + f"step_{epoch}")
+    if script_args.save_freq and epoch and epoch % script_args.save_freq == 0:
+        ppo_trainer.save_pretrained(script_args.output_dir + f"step_{epoch}")
