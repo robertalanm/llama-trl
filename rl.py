02b70bf7c348c13b7fcb236b90306fe722403dd8
Carro <robertmyers@hey.com>
2023-09-23 20:18:14 -0500

    a

--- rl.py
+++ rl.py
@@ -33,7 +33,8 @@ class MyRLEnv(TextRLEnv):
         self.reward_model = OpenAssistantRewardModel("cuda")
         
     def get_reward(self, input_item, predicted_list, finish):  # predicted will be the list of predicted token
-        rewards = self.reward_model.get_rewards(input_item, predicted_list, "test")
+        p_list = [item['input'] for item in predicted_list]
+        rewards = self.reward_model.get_rewards(input_item, p_list, "test")
         print(rewards)
         if finish:
             reward = [1]  # calculate reward score base on predicted_list
