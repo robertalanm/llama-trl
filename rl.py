a6285683de5b0159550caa15e426f5c35e94ce11
Carro <robertmyers@hey.com>
2023-09-24 00:49:43 -0500

    a

--- rl.py
+++ rl.py
@@ -7,6 +7,7 @@ import pandas as pd
 from reward.open_assistant import OpenAssistantRewardModel
 import wandb
 from tqdm import tqdm
+import torch
 
 from typing import List
 # from accelerate import Accelerator
