99907f14cf32c84cfa1e175e08136c78d9fb889b
Carro <robertmyers@hey.com>
2023-09-24 00:36:22 -0500

    a

--- rl.py
+++ rl.py
@@ -27,7 +27,7 @@ print( df["prompt"])
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 model = AutoModelForCausalLM.from_pretrained(checkpoint, revision=revision, torch_dtype="auto")
 
-# model.to("cuda")
+model.to("cuda")
 
 class MyRLEnv(TextRLEnv):
     def __init__(self, model, tokenizer, observation_input, max_length, compare_sample, **kwargs):
@@ -60,7 +60,7 @@ actor = TextRLActor(env, model, tokenizer,
                     top_p=1.0,)
 agent = actor.agent_ppo(update_interval=10, minibatch_size=3000, epochs=10)
 
-env, actor, agent, observation_list model = accelerator.prepare(env, actor, agent, observation_list, model)
+env, actor, agent, observation_list, model = accelerator.prepare(env, actor, agent, observation_list, model)
 # print(observation_list[0]['input'])
 # print(actor.predict(observation_list[0]))
 
