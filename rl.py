0565f9a475ecbbb1cdd8cb9eb423a19ad6793395
Carro <robertmyers@hey.com>
2023-09-23 19:05:02 -0500

    a

--- rl.py
+++ rl.py
@@ -17,7 +17,7 @@ model = AutoModelForCausalLM.from_pretrained(checkpoint, revision=revision, torc
 model = model.cuda()
 
 # dataset
-df = pd.read_parquer("./data/aa.parquet")
+df = pd.read_parquet("./data/aa.parquet")
 
 observation_list = [{"input": prompt} for prompt in df["prompt"]][:100]
 
