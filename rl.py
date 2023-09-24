00e3daa28dd49eb7c4c8d5d837d8bc82d6375e08
Carro <robertmyers@hey.com>
2023-09-23 22:32:49 -0500

    a

--- rl.py
+++ rl.py
@@ -41,7 +41,7 @@ class MyRLEnv(TextRLEnv):
         
         if finish:
             reward = [1] * len(predicted_list)  # calculate reward score base on predicted_list
-        return list(rewards)
+        return list(rewards.cpu())
 
 
 # observaton_list = [{"input":"explain how attention work in seq2seq model"}]
