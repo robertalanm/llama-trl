4e31a5731b88f7d67b7a3c29a109503484800008
Carro <robertmyers@hey.com>
2023-09-23 19:26:35 -0500

    a

--- rl.py
+++ rl.py
@@ -43,7 +43,7 @@ actor = TextRLActor(env, model, tokenizer,
                     top_k=0,
                     top_p=1.0,)
 agent = actor.agent_ppo(update_interval=2, minibatch_size=2, epochs=10)
-print(actor.predict(observaton_list[0]))
+print(actor.predict(observation_list[0]))
 
 train_agent_with_evaluation(
     agent,
